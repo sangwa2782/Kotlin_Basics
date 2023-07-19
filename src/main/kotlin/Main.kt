fde62cc514f7eec345f9fb7038be8143917f4bd7
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-07-19 13:16:04 +0530

    Number swapping

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,16 @@
 import java.util.Scanner
 
 fun main(args: Array<String>) {
-   val dividend = 25
-    val divisor = 4
+    var a = 5
+    var b = 6
 
-    var quotient = dividend / divisor
+    //swapping where b=5 & a=6
+    var c = a
 
-    var remainder = dividend % divisor
+    a = b
 
-    println("Quotient = $quotient")
-    println("Remainder = $remainder")
+    b = c
+
+    println("a = $a")
+    println("b = $b")
 }
