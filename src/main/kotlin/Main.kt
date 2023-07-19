2cc00ee52fdca9566db0b495b7b7c8f052cdc69b
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-07-19 15:20:42 +0530

    Fibonacci series

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,18 +3,12 @@ import java.util.Scanner
 fun main(args: Array<String>) {
 //  Kotlin Program to Display Fibonacci Series
 
-  val num = 10
-  var t1 = 0
-  var t2 = 1
+ var a = 'a'
 
-  for (i in 1..num){
-    print("$t1 + ")
-
-    val sum = t1 + t2
-    t1 = t2
-    t2 = sum
-
-
+  while (a <='z') {
+    print("$a")
+    ++a
   }
+
 
 }
