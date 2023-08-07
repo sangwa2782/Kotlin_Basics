ae5415d6fef721fd69f628c88f7a0ab9eddf053d
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-08-07 19:04:42 +0530

    Check given number is letter digit or special charecter

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,19 @@
 import java.util.Scanner
 
+//Check given number is letter digit or special charecter
 fun main(args: Array<String>) {
 //Find Factorial of a number using for loop
+    val reader = Scanner(System.`in`)
+    print("Enter a character: ")
 
-    var reader = Scanner(System.`in`)
-    print("Enter the number: ")
+    val a = reader.next()[0]
 
-    var num = reader.nextInt()
+    when {
+        a.isLetter()-> println("Letter")
+        a.isDigit()-> println("Digit")
+        else-> println("Special Charecter")
+    }
 
-    var num2  = num.toInt()
-//    var num = 2547856
 
-    var factorial = 1
-
-    for (i in 1..num2)
-
-        factorial *= i
-
-    println("Number of digits: $factorial")
-
+    
 }
