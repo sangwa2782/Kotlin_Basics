cd9e3b2aaa71f27555fb634579e8e54e1a2a804f
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-08-07 19:53:56 +0530

    Function to check two strings are anagram string or not

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,50 @@
 import java.lang.StringBuilder
-import java.util.Scanner
+import java.util.*
 
-//Program to count the number of words in a string in Kotlin
+//Function to check two strings are anagram string or not
 
 
-fun countWords(inputString: String): Int {
-    val strArray = inputString.split(" ".toRegex()).toTypedArray()
-
-    var count = 0
-
-    for (s in strArray) {
-        if (s != "") {
-            count++
-        }
-    }
-    return count
-}
 
 fun main(args: Array<String>) {
-//Kotlin program to find total number of vowels, consonants, digits and spaces in a string
-    val reader = Scanner(System.`in`)
-    print("Enter a String: ")
-    val a:String = reader.nextLine()
+//Function to check two strings are anagram string or not
+    val sc = Scanner(System.`in`)
 
+    //Enter String Value
+    println("Enter String1 : ")
+    val str1: String = sc.nextLine()
 
+    println("Enter String2 : ")
+    val str2: String = sc.next()
 
-    //Print words Count in a String
-    println("No. of Words in String : " + countWords(a))
+    //Call Function to  check anagram String
+    if (isAnagrams(str1, str2)) {
+        println("Anagram Strings !!")
+    } else {
+        println("Strings are not Anagram !!")
+    }
+}
+
+fun isAnagrams(str1: String, str2: String): Boolean {
+    //Both String Length must be Equal
+    if (str1.length != str2.length) {
+        return false
+    }
+
+    //Convert Strings to character Array
+    val strArray1 = str1.toCharArray()
+    val strArray2 = str2.toCharArray()
+
+    //Sort the Arrays
+    Arrays.sort(strArray1)
+    Arrays.sort(strArray2)
+
+    //Convert Arrays to String
+    val sortedStr1 = String(strArray1)
+    val sortedStr2 = String(strArray2)
+
+    //Check Both String Equals or not After Sorting
+    //and Return value True or False
+    return sortedStr1 == sortedStr2
 }
 
 
