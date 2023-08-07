d327ec6f477c888f9ef51c5e7deaf788cdec5914
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-08-07 19:54:48 +0530

    Palindrom word

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,50 +1,26 @@
 import java.lang.StringBuilder
-import java.util.*
+import java.util.Scanner
 
-//Function to check two strings are anagram string or not
+//Palindrom word
 
 
 
 fun main(args: Array<String>) {
-//Function to check two strings are anagram string or not
-    val sc = Scanner(System.`in`)
 
-    //Enter String Value
-    println("Enter String1 : ")
-    val str1: String = sc.nextLine()
+    val reader = Scanner(System.`in`)
+    print("Enter a String: ")
+    var a = reader.next()
 
-    println("Enter String2 : ")
-    val str2: String = sc.next()
 
-    //Call Function to  check anagram String
-    if (isAnagrams(str1, str2)) {
-        println("Anagram Strings !!")
-    } else {
-        println("Strings are not Anagram !!")
+    if (a == a.reversed()) {
+        println("Your word is palindrom" )
     }
-}
-
-fun isAnagrams(str1: String, str2: String): Boolean {
-    //Both String Length must be Equal
-    if (str1.length != str2.length) {
-        return false
+    else {
+        println("Your word is not palindrom" )
     }
 
-    //Convert Strings to character Array
-    val strArray1 = str1.toCharArray()
-    val strArray2 = str2.toCharArray()
+    //Print words Count in a String
 
-    //Sort the Arrays
-    Arrays.sort(strArray1)
-    Arrays.sort(strArray2)
-
-    //Convert Arrays to String
-    val sortedStr1 = String(strArray1)
-    val sortedStr2 = String(strArray2)
-
-    //Check Both String Equals or not After Sorting
-    //and Return value True or False
-    return sortedStr1 == sortedStr2
 }
 
 
