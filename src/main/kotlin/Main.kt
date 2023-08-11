280831d8f70c0833da685225ec92852638a2e494
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-08-11 10:03:50 +0530

    kotlin method overloading Funtions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,35 +1,16 @@
 fun main(args: Array<String>) {
-
-    var result = add(4,5)
-    println(result)
-    val result2 = add(6,7)
-    println(result2)
-
-    evenOdd(8)
-    evenOdd(11)
-
-    printMessage(5)
-    printMessage()
-
+    //funtion using method overloading
+    println(addition(5,6))
+    println(addition(5.0,6.0))
 }
 
-//Funtions are the set of instructions that perform a specific task
 
-// Simple Add function
-fun add(x:Int, y:Int):Int = x + y
-
-
-// Function to check number is even of odd
-fun evenOdd(x: Int) {
-    val result = if (x % 2 == 0) "Even" else "Odd"
-    println(result)
-    //in kotlin, when there is no return type then it has inbuilt return type is Unit.
-
+fun addition(x:Int, y:Int):Int
+{
+    return x+y
 }
 
-//print message with default argument(Int = 2)
-fun printMessage(count: Int = 2) {
-    for (i in 1..count) {
-        println("Hello $i")
-    }
+fun addition(x:Double, y:Double):Double
+{
+    return x+y
 }
