e124c92eeeeadee1fb8c536aab9cda9f517d527c
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-08-11 14:16:17 +0530

    kotlin Abstract Class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,31 @@
 import kotlin.math.pow
 
+
+
+
+// ABSTRACT
+// * A class can't be instantiated
+// * Abstract class can have both abstract and non abstract properties & method
+// * If a class has abstract method or property then class must be declared as abstract
+
 fun main(args: Array<String>) {
-// METHOD polymorphism and inheritance
+    val circle = Circles(4.0)
+    println(circle.area())
 
-    val circle : Shape = Circle(4.0)
-    val square : Shape = Square(6.0)
+    circle.display()
 
-    val shapes = arrayOf(Circle(4.0), Square(4.0), Triangle(3.0, 4.0))
-    printArea(shapes)
 }
 
-fun printArea(shapes: Array<Shape>){
-    for (shape in shapes){
-        println(shape.area())
+abstract class Shape{
+    var name: String = ""
+    abstract  fun area() : Double
+    abstract  fun display()
+}
+
+class Circles(val radius:Double) : Shape(){
+    override fun area(): Double = Math.PI * radius * radius
+    override fun display() {
+       println("Circle is getting displayed")
     }
 }
 
-open class Shape{
-    open fun area() : Double = 0.0
-}
-
-class Circle(val radius:Double):Shape(){
-    override fun area(): Double = Math.PI * radius * radius
-}
-
-class  Square(val side:Double) : Shape() {
-    override fun area(): Double = side * side
-}
-
-class Triangle(val base:Double, val height:Double) : Shape() {
-    override fun area(): Double = 0.5 * base * height
-}
