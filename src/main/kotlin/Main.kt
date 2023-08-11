e4594115807c6982f0e3f05818696c37dc65cf03
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-08-11 09:54:27 +0530

    kotlin Funtions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,35 @@
 fun main(args: Array<String>) {
-   for (i in 1..10) {
-       println("$i")
-   }
 
-     println("using step 2")
-    for (i in 1..10 step 2){
-        println("$i")
+    var result = add(4,5)
+    println(result)
+    val result2 = add(6,7)
+    println(result2)
+
+    evenOdd(8)
+    evenOdd(11)
+
+    printMessage(5)
+    printMessage()
+
+}
+
+//Funtions are the set of instructions that perform a specific task
+
+// Simple Add function
+fun add(x:Int, y:Int):Int = x + y
+
+
+// Function to check number is even of odd
+fun evenOdd(x: Int) {
+    val result = if (x % 2 == 0) "Even" else "Odd"
+    println(result)
+    //in kotlin, when there is no return type then it has inbuilt return type is Unit.
+
+}
+
+//print message with default argument(Int = 2)
+fun printMessage(count: Int = 2) {
+    for (i in 1..count) {
+        println("Hello $i")
     }
-
-
-    println("using until")
-    for (i in 1 until 6) {
-        println("$i")
-    }
-
-    //we can also apply step method on reverse counting
-    println("Reverse Counting")
-    for (i in 10 downTo 2) {
-        println("$i")
-    }
-
-    println("Create a Table")
-    val x = 5
-    for (i in 1..10){
-        println("$x * $i = ${x*i}")
-    }
-
-
 }
