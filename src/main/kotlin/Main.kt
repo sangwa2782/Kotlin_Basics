dade7f3b296a6449ab0bd2d0740cc68e9bcc7cc8
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-08-11 10:09:18 +0530

    kotlin method Hiergher Order Funtions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,24 @@
+import kotlin.math.pow
+
 fun main(args: Array<String>) {
-    //funtion using method overloading
-    println(addition(5,6))
-    println(addition(5.0,6.0))
+
+    //funtion using hiegher order function
+
+    var fn: (a:Double, b:Double)->Double=::addition
+    println(fn (1.0,2.0))
+
+    fn = ::power
+    println(fn(2.0,3.0))
+
 }
 
-
-fun addition(x:Int, y:Int):Int
-{
-    return x+y
-}
 
 fun addition(x:Double, y:Double):Double
 {
     return x+y
+}
+
+fun power(a:Double, b:Double):Double
+{
+    return a.pow(b)
 }
