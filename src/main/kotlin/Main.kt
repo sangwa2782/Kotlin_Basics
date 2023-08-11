ab8cc7ad6aff3e4a8be1d19e9adb2f30017ee030
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-08-11 09:10:19 +0530

    do while condition

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,12 @@
 fun main(args: Array<String>) {
-    val x = "Dog"
+    var x = 7
 
-    when (x) {
-        "Cat" -> println("This is Cat")
-        "Rat" -> println("This is Rat")
-        "Yak" -> println("This is Yak")
-        "Dog" -> println("This is dog")
-        else -> println("Animal Not Found")
+    do {
 
+        print("Hello")
+        x++
 
-    }
+    }while (x<10)
 
 
 
