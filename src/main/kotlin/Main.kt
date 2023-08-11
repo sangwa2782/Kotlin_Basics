1d6b6266a5e360b2418ed51de3b14f1b92e03140
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-08-11 10:27:26 +0530

    kotlin Array

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,35 @@
 import kotlin.math.pow
 
 fun main(args: Array<String>) {
+    //kotlin Array manipulation
 
-    //funtion using hiegher order function
+    var arr = arrayOf("One","Two","Three")
+    var arr2 = arrayOf(1,2,3,4,5,6)
+    var arr3  = arrayOf<Int>(1,2,5,6,8)
 
-    var fn: (a:Double, b:Double)->Double=::addition
-    println(fn (1.0,2.0))
+    println("for simple array")
+    for (i in arr) {
+        println("$i")
+    }
 
-    fn = ::power
-    println(fn(2.0,3.0))
+    println("for array with index")
+    for ((i,e) in arr.withIndex()) {
+        println("$i - $e")
+    }
 
-}
+    println(" print with selected data")
+    println("${arr[2]}")
+    println("${arr.get(1)}")
+    arr.set(0,"Five")
+
+    println(arr[1])
+    println(arr.size)
+
+//    for (i in arr) {
+//        println("$i")
+//    }
 
 
-fun addition(x:Double, y:Double):Double
-{
-    return x+y
-}
 
-fun power(a:Double, b:Double):Double
-{
-    return a.pow(b)
+
 }
