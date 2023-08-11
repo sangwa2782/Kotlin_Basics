259aaca2acbb3d59745fd7c22480c35fb1c4ea92
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-08-11 09:17:59 +0530

    for loop with other features condition

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,24 @@
 fun main(args: Array<String>) {
-    var x = 7
+//   for (i in 1..10) {
+//       println("$i")
+//   }
 
-    do {
-
-        print("Hello")
-        x++
-
-    }while (x<10)
+     println("using step 2")
+    for (i in 1..10 step 2){
+        println("$i")
+    }
 
 
+    println("using until")
+    for (i in 1 until 6) {
+        println("$i")
+    }
 
+    //we can also apply step method on reverse counting
+    println("Reverse Counting")
+    for (i in 10 downTo 2) {
+        println("$i")
+    }
 
 
 }
