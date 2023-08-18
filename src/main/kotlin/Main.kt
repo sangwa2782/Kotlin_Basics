a494446b7dd214516529ba9db09f06bc7e9d5901
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-08-18 16:34:40 +0530

    2:- remove duplicate element from array in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,16 +4,22 @@ import kotlin.math.pow
 
 
 
-//1:- 2:- Write a program to remove duplicate element from array in kotlin
+//1:- Write a program to sort element of array in kotlin
 
 
 
 fun main(args: Array<String>) {
     val array = arrayOf(6,3,5,3,2,5,1,2,2,3,4,6,6,6,5,5,1,4,10,7)
 
+    //------------- using function ---------------//
+    val data = removeDuplicates(array)
+    println(data.contentToString())
+    //------------------- view function ----------//
+
+
 // 1 using distinction()
-    var data = array.distinct()
-    println(data)
+//    var data = array.distinct()
+//    println(data)
 
 //2 using toSet()
 //    var data = array.toSet()
@@ -28,6 +34,12 @@ fun main(args: Array<String>) {
 //    println(data)
 
 }
+
+fun removeDuplicates(array: Array<Int>): IntArray {
+    return array.distinct().toIntArray()
+}
+
+
 
 
 
