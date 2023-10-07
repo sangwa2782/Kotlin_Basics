0e465432d7e2b4d99429d24d51a0ac309d502a78
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-10-07 13:10:01 +0530

    Program to input a string in Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,32 @@
 import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.math.hypot
 
-//Program to calculate and display student grades in Kotlin
+//Program to input a string in Kotlin
 
 fun main() {
-    var read = Scanner(System.`in`)
+//    var read = Scanner(System.`in`)
 
-    val subjectName = arrayOf<String>("Hindi","English","Physics","Chemistry","Math")
+    println("Enter your name: ")
+    val name = readln()
 
-    val marksArray = DoubleArray(5)
+    println("Enter your name: ")
+    val address = readln()
 
-    println("Enter marks: ")
-    for (i in marksArray.indices) {
-        print("${subjectName[i]} : ")
-        marksArray[i] = read.nextDouble()
-    }
+    println("Enter your name: ")
+    val country = readln()
 
-    val total = marksArray.sum()
-    var percentage = marksArray.average()
-
-    println("Total marks : $total")
-    println("Percentage : $percentage")
-
-    when {
-        percentage > 80 -> println("A Grade")
-        percentage > 60 -> println("B Grade")
-        percentage > 40 -> println("C Grade")
-    }
-
-
-
+    println("Your name is : $name")
+    println("Your Address is : $address")
+    println("Your country name is : $country")
 
 }
 
 
-
-
-
+fun largestNum(a:Int, b:Int, c:Int):Int {
+       return when {
+            a>b && a>c -> a
+            b>c -> b
+            else -> c
+        }
+}
